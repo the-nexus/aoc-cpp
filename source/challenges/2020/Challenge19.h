1a57313ee5b10e0a421af29e597ac504ccfcb012
the-nexus <kevin.pulgarturcotte@gmail.com>
2022-11-25 19:22:01 -0500

    Created all code files and downloaded all inputs for all the available days up to now

---
+++ source/challenges/2020/Challenge19.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../ChallengeAbstract.h"
+
+namespace Year2020
+{
+    class Challenge19 : public ChallengeAbstract
+    {
+    public:
+        virtual void SetUp(std::vector<std::string> const& inputLines) override
+        {
+
+        }
+
+        virtual void Run_PartOne() override
+        {
+            std::cout << "WARNING: Part One Not Implemented" << std::endl;
+        }
+
+        virtual void Run_PartTwo() override
+        {
+            std::cout << "WARNING: Part Two Not Implemented" << std::endl;
+        }
+
+        virtual void CleanUp() override
+        {
+
+        }
+    };
+}
