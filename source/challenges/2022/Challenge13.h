466cf06558033dbc2a418153833fa35eead9e0a4
the-nexus <the-nexus@github.com>
2022-12-13 17:55:36 -0500

    Continued some work on 2022/13

--- source/challenges/2022/Challenge13.h
+++ source/challenges/2022/Challenge13.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "../ChallengeAbstract.h"
-#include <memory>
+#include <iostream>
 
 namespace Year2022
 {
@@ -17,20 +17,114 @@ namespace Year2022
 
         struct TreeNode
         {
-            virtual bool IsVectorNode() const { return false; }
-            virtual bool IsIntegerNode() const { return false; }
+            virtual void Destroy() { delete this; }
+            virtual EOrderResult CompareOrder(TreeNode const* rhs) const = 0;
         };
 
         struct TreeNode_Vector final : public TreeNode
         {
-            virtual bool IsVectorNode() const override { return true; }
+            virtual void Destroy() override
+            {
+                for (TreeNode* childNode : m_childNodes)
+                {
+                    childNode->Destroy();
+                }
+
+                TreeNode::Destroy();
+            }
+
+            virtual EOrderResult CompareOrder(TreeNode const* rhs) const override
+            {
+                if (TreeNode_Integer const* rhsInteger = dynamic_cast<TreeNode_Integer const*>(rhs))
+                {
+                    TreeNode_Vector rhsVector;
+                    rhsVector.m_childNodes.push_back(const_cast<TreeNode_Integer*>(rhsInteger));
+                    return CompareOrder(&rhsVector);
+                }
+                else if (TreeNode_Vector const* rhsVector = dynamic_cast<TreeNode_Vector const*>(rhs))
+                {
+                    return CompareOrder(rhsVector);
+                }
+                else
+                {
+                    return EOrderResult::Unknown;
+                }
+            }
+
+            EOrderResult CompareOrder(TreeNode_Vector const* rhs) const
+            {
+                bool const isLhsSmaller = m_childNodes.size() < rhs->m_childNodes.size();
+                size_t const commonSize = isLhsSmaller ? m_childNodes.size() : rhs->m_childNodes.size();
+
+                for (size_t nodeIndex = 0; nodeIndex < commonSize; ++nodeIndex)
+                {
+                    EOrderResult const orderResult = m_childNodes[nodeIndex]->CompareOrder(rhs->m_childNodes[nodeIndex]);
+                    if (orderResult != EOrderResult::Unknown)
+                    {
+                        return orderResult;
+                    }
+                }
+                
+                if (m_childNodes.size() < rhs->m_childNodes.size())
+                {
+                    return EOrderResult::Unknown;
+                }
+                else
+                {
+                    return isLhsSmaller ? EOrderResult::Ordered : EOrderResult::NotOrdered;
+                }
+            }
+
             std::vector<TreeNode*> m_childNodes;
+
+            std::ostream& operator<<(std::ostream& os)
+            {
+                os << "[";
+                for (size_t nodeIndex = 0; nodeIndex < m_childNodes.size(); ++nodeIndex)
+                {
+                    if (nodeIndex != 0)
+                    {
+                        os << ',';
+                    }
+                    os << m_childNodes[nodeIndex];
+                }
+                os << "]";
+                return os;
+            }
         };
 
         struct TreeNode_Integer final : public TreeNode
         {
-            virtual bool IsIntegerNode() const override { return true; }
+            virtual EOrderResult CompareOrder(TreeNode const* rhs) const override
+            {
+                if (TreeNode_Integer const* rhsInteger = dynamic_cast<TreeNode_Integer const*>(rhs))
+                {
+                    return CompareOrder(rhsInteger);
+                }
+                else if (TreeNode_Vector const* rhsVector = dynamic_cast<TreeNode_Vector const*>(rhs))
+                {
+                    TreeNode_Vector lhsVector;
+                    lhsVector.m_childNodes.push_back(const_cast<TreeNode_Integer*>(this));
+                    return lhsVector.CompareOrder(rhsVector);
+                }
+                else
+                {
+                    return EOrderResult::Unknown;
+                }
+            }
+
+            EOrderResult CompareOrder(TreeNode_Integer const* rhs) const
+            {
+                return m_value < rhs->m_value ? EOrderResult::Ordered : EOrderResult::Unknown;
+            }
+
             int m_value = 0;
+
+            std::ostream& operator<<(std::ostream& os)
+            {
+                os << m_value;
+                return os;
+            }
         };
 
         std::vector<std::pair<TreeNode*, TreeNode*>> m_nodePairs;
@@ -39,17 +133,32 @@ namespace Year2022
         virtual void SetUp(std::vector<std::string> const& inputLines) override
         {
             m_nodePairs.reserve((inputLines.size() + 1) / 3);
-            for (size_t lineIndex = 0; lineIndex < inputLines.size(); ++lineIndex)
+            for (size_t lineIndex = 0; lineIndex < 1/*inputLines.size()*/; ++lineIndex)
             {
-                std::pair<TreeNode*, TreeNode*>& pair = m_nodePairs.emplace_back();
-                pair.first = MakeTree(inputLines[lineIndex]);
-                pair.second = MakeTree(inputLines[lineIndex + 1]);
+                TreeNode* node = MakeTree(inputLines[lineIndex]);
+                std::cout << node << std::endl;
+                delete node;
+
+                // std::pair<TreeNode*, TreeNode*>& pair = m_nodePairs.emplace_back();
+                // pair.first = MakeTree(inputLines[lineIndex]);
+                // pair.second = MakeTree(inputLines[lineIndex + 1]);
+
+                // std::cout << pair.first << std::endl;
+                // std::cout << pair.second << std::endl << std::endl;
             }
         }
 
         virtual void Run_PartOne() override
         {
-            std::cout << "WARNING: Part One Not Implemented" << std::endl;
+            size_t orderedPairIndexSum = 0;
+            for (size_t pairIndex = 0; pairIndex < m_nodePairs.size(); ++pairIndex)
+            {
+                if (m_nodePairs[pairIndex].first->CompareOrder(m_nodePairs[pairIndex].second) == EOrderResult::Ordered)
+                {
+                    orderedPairIndexSum += pairIndex + 1;
+                }
+            }
+            std::cout << orderedPairIndexSum << std::endl;
         }
 
         virtual void Run_PartTwo() override
@@ -61,56 +170,50 @@ namespace Year2022
         {
             for (std::pair<TreeNode*, TreeNode*>& nodePair : m_nodePairs)
             {
-                DestroyTreeNode(nodePair.first);
-                DestroyTreeNode(nodePair.second);
+                nodePair.first->Destroy();
+                nodePair.second->Destroy();
             }
         }
 
     private:
         TreeNode* MakeTree(std::string const& inputLine) const
         {
-            return nullptr;
+            size_t inputCharIndex = 0;
+            return MakeVectorNode(inputLine, inputCharIndex);
         }
 
-        void DestroyTreeNode(TreeNode_Vector* vectorNode) const
+        TreeNode_Vector* MakeVectorNode(std::string const& inputLine, size_t& inputCharIndex) const
         {
-
-        }
-
-        void DestroyTreeNode(TreeNode_Integer* integerNode) const
-        {
-            delete integerNode;
-        }
-
-        int CompareOrder(TreeNode_Integer* lhs, TreeNode_Integer rhs) const
-        {
-            if (lhs->m_value == rhs.m_value)
+            TreeNode_Vector* vectorNode = new TreeNode_Vector;
+            while (inputLine[++inputCharIndex] != ']')
             {
-                return 0;
+                std::cout << inputLine[inputCharIndex];
+                if (inputLine[inputCharIndex] == '[')
+                {
+                    vectorNode->m_childNodes.push_back(MakeVectorNode(inputLine, inputCharIndex));
+                }
+                else if (inputLine[inputCharIndex] == ',')
+                {
+                    continue;
+                }
+                else
+                {
+                    vectorNode->m_childNodes.push_back(MakeIntegerNode(inputLine, inputCharIndex));
+                }
             }
-            else
+            return vectorNode;
+
+        }
+
+        TreeNode_Integer* MakeIntegerNode(std::string const& inputLine, size_t& inputCharIndex) const
+        {
+            TreeNode_Integer* integerNode = new TreeNode_Integer;
+            while (inputLine[inputCharIndex] != ',' && inputLine[inputCharIndex] != ']')
             {
-                return lhs->m_value < rhs.m_value ? -1 : 1;
+                int const charValue = static_cast<int>(inputLine[inputCharIndex++] - '0');
+                integerNode->m_value = integerNode->m_value * 10 + charValue;
             }
-        }
-
-        int CompareOrder(TreeNode_Vector* lhs, TreeNode_Vector* rhs) const
-        {
-            return 0;
-        }
-
-        int CompareOrder(TreeNode_Integer* lhs, TreeNode_Vector* rhs) const
-        {
-            TreeNode_Vector lhsVectorNode;
-            lhsVectorNode.m_childNodes.push_back(lhs);
-            return Compare(&lhsVectorNode, rhs);
-        }
-
-        int CompareOrder(TreeNode_Vector* lhs, TreeNode_Integer* rhs) const
-        {
-            TreeNode_Vector rhsVectorNode;
-            rhsVectorNode.m_childNodes.push_back(lhs);
-            return Compare(lhs, &rhsVectorNode);
+            return integerNode;
         }
     };
 }
