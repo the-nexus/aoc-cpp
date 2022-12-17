2b49f489ff3d5936874120d855b298258084debf
the-nexus <kevin.pulgarturcotte@gmail.com>
2022-12-16 19:01:37 -0500

    Completed day 16 of 2022

--- source/challenges/2022/Challenge16.h
+++ source/challenges/2022/Challenge16.h
@@ -10,12 +10,32 @@ namespace Year2022
     class Challenge16 : public ChallengeAbstract
     {
     private:
-        std::map<int, int> m_rates;
-        std::map<int, std::vector<int>> m_tunnels;
+        struct Worker
+        {
+            Worker(int const startingValveId)
+                : m_currentValveId(startingValveId)
+                , m_currentRate(0)
+                , m_remainingTravelTime(0)
+                , m_releasedPressure(0)
+            {}
+
+            int m_currentValveId = 0;
+            int m_currentRate = 0;
+            int m_remainingTravelTime = 0;
+            int m_releasedPressure = 0;
+        };
+
+        int m_startingValveId = 0;
+        std::set<int> m_workingValves;
+        std::map<int, int> m_valveRates;
+        std::map<int, std::map<int, int>> m_directPaths;
 
     public:
         virtual void SetUp(std::vector<std::string> const& inputLines) override
         {
+            m_startingValveId = GetValveId('A', 'A');
+
+            std::map<int, std::vector<int>> paths;
             for (std::string const& inputLine : inputLines)
             {
                 size_t const srcLabelIndex = inputLine.find_first_of(' ') + 1;
@@ -23,38 +43,64 @@ namespace Year2022
                 size_t const rateEndIndex = inputLine.find_first_of(';', rateStartIndex);
                 size_t const dstLabelsIndex = inputLine.find_first_of(' ', inputLine.find_last_of("valve")) + 1;
 
-                //std::cout << inputLine.substr(srcLabelIndex, 2) << "|" << inputLine.substr(rateStartIndex, rateEndIndex - rateStartIndex) << "|" << inputLine.substr(dstLabelsIndex) << std::endl;
-
                 std::vector<std::string> dstLabels;
                 StringTool::SplitString(inputLine.substr(dstLabelsIndex), ", ", dstLabels);
 
+                // Parse the info of the current valve
                 int const srcId = GetValveId(inputLine[srcLabelIndex], inputLine[srcLabelIndex + 1]);
-                m_rates[srcId] = atoi(inputLine.substr(rateStartIndex, rateEndIndex - rateStartIndex).c_str());
-                m_tunnels[srcId].reserve(dstLabels.size());
+                int const srcRate = atoi(inputLine.substr(rateStartIndex, rateEndIndex - rateStartIndex).c_str());
+                m_valveRates[srcId] = srcRate;
+                if (srcRate > 0)
+                {
+                    m_workingValves.insert(srcId);
+                }
 
+                // Parse the destinations of the current valve
+                paths[srcId].reserve(dstLabels.size());
                 for (std::string const& dstLabel : dstLabels)
                 {
-                    m_tunnels[srcId].push_back(GetValveId(dstLabel[0], dstLabel[1]));
+                    paths[srcId].push_back(GetValveId(dstLabel[0], dstLabel[1]));
+                }
+            }
+
+            // Map all the shortest path costs between the working valves (and starting valve towards working valves) to simplify execution
+            int pathCost = 0;
+            for (int const dstValveId : m_workingValves)
+            {
+                if (FindDirectPathCost(m_startingValveId, dstValveId, pathCost, paths))
+                {
+                    m_directPaths[m_startingValveId][dstValveId] = pathCost;
+                }
+
+                for (int const srcValveId : m_workingValves)
+                {
+                    if (srcValveId == dstValveId)
+                    {
+                        continue;
+                    }
+
+                    if (FindDirectPathCost(srcValveId, dstValveId, pathCost, paths))
+                    {
+                        m_directPaths[srcValveId][dstValveId] = pathCost;
+                    }
                 }
             }
         }
 
         virtual void Run_PartOne() override
         {
-            int const originValveId = GetValveId('A', 'A');
+            std::set<int> closedValves = m_workingValves;
+            std::vector<Worker> workers = { Worker(m_startingValveId) };
 
-            std::set<int> workingValves;
-            FindWorkingValves(workingValves);
-
-            std::map<int, std::map<int, int>> directPaths;
-            MapDirectPathsToImportantValves(originValveId, workingValves, directPaths);
-
-            std::cout << FindBestPressureReleaseAmount(0, 30, originValveId, workingValves, directPaths) << std::endl;
+            std::cout << FindBestPressureRelease(30, workers, closedValves) << std::endl;
         }
 
         virtual void Run_PartTwo() override
         {
-            std::cout << "WARNING: Part Two Not Implemented" << std::endl;
+            std::set<int> closedValves = m_workingValves;
+            std::vector<Worker> workers = { Worker(m_startingValveId), Worker(m_startingValveId) };
+
+            std::cout << FindBestPressureRelease(26, workers, closedValves) << std::endl;
         }
 
         virtual void CleanUp() override {}
@@ -62,65 +108,20 @@ namespace Year2022
     private:
         int GetValveId(char const firstValveLabelChar, char const secondValveLabelChar) const
         {
-            return static_cast<int>(firstValveLabelChar - 'A') * 100 + static_cast<int>(secondValveLabelChar - 'A');
+            return (static_cast<int>(firstValveLabelChar - 'A') + 1) * 100 + static_cast<int>(secondValveLabelChar - 'A') + 1;
         }
 
-        std::string GetValveLabel(int const valveId) const
-        {
-            std::string label = "##";
-            label[0] = static_cast<char>(valveId / 100) + 'A';
-            label[1] = static_cast<char>(valveId % 100) + 'A';
-            return label;
-        }
-
-        void FindWorkingValves(std::set<int>& outWorkingValves) const
-        {
-            outWorkingValves.clear();
-            for (std::pair<int, int> const& ratePair : m_rates)
-            {
-                if (ratePair.second > 0)
-                {
-                    outWorkingValves.insert(ratePair.first);
-                }
-            }
-        }
-
-        void MapDirectPathsToImportantValves(int const originValveId, std::set<int> const& workingValves, std::map<int, std::map<int, int>>& directPaths) const
-        {
-            int pathCost = 0;
-            for (int const dstValveId : workingValves)
-            {
-                if (FindShortestPath(originValveId, dstValveId, pathCost))
-                {
-                    directPaths[originValveId][dstValveId] = pathCost;
-                }
-
-                for (int const srcValveId : workingValves)
-                {
-                    if (srcValveId == dstValveId)
-                    {
-                        continue;
-                    }
-
-                    if (FindShortestPath(srcValveId, dstValveId, pathCost))
-                    {
-                        directPaths[srcValveId][dstValveId] = pathCost;
-                    }
-                }
-            }
-        }
-
-        bool FindShortestPath(int const srcValveId, int const dstValveId, int& outPathCost) const
+        bool FindDirectPathCost(int const srcValveId, int const dstValveId, int& outPathCost, std::map<int, std::vector<int>> const& paths) const
         {
             std::set<int> visitedValveIds;
             std::set<int> currentLayer;
             std::set<int> nextLayer;
 
-            std::vector<int> const& srcDirectConnections = m_tunnels.find(srcValveId)->second;
+            std::vector<int> const& srcDirectConnections = paths.find(srcValveId)->second;
             currentLayer.insert(srcDirectConnections.begin(), srcDirectConnections.end());
 
             outPathCost = 0;
-            while (visitedValveIds.size() < m_tunnels.size())
+            while (visitedValveIds.size() < paths.size())
             {
                 ++outPathCost;
                 for (int const& currentValveId : currentLayer)
@@ -132,7 +133,7 @@ namespace Year2022
                             return true;
                         }
 
-                        std::vector<int> const& directConnections = m_tunnels.find(currentValveId)->second;
+                        std::vector<int> const& directConnections = paths.find(currentValveId)->second;
                         nextLayer.insert(directConnections.begin(), directConnections.end());
 
                         visitedValveIds.insert(currentValveId);
@@ -147,29 +148,64 @@ namespace Year2022
             return false;
         }
 
-        int FindBestPressureReleaseAmount(int const currentPressureAmount, int const remainingTime, int const currentValveId, std::set<int>& closedValves, std::map<int, std::map<int, int>> const& directPaths) const
+        int FindBestPressureRelease(int const remainingTime, std::vector<Worker> workers, std::set<int>& closedValves) const
         {
-            if (remainingTime <= 0 || closedValves.size() == 0)
+            // Wait for any worker to be available
+            int nextRemainingTime = remainingTime;
+            while (nextRemainingTime > 0 && (closedValves.size() == 0 || !IsAnyWorkerAvailable(workers)))
             {
-                return currentPressureAmount;
+                --nextRemainingTime;
+                for (Worker& worker : workers)
+                {
+                    --worker.m_remainingTravelTime;
+                    worker.m_releasedPressure += worker.m_currentRate;
+
+                    if (worker.m_remainingTravelTime == 0)
+                    {
+                        // Integrate the current valve's rate now that the travel time is achieved
+                        worker.m_currentRate += m_valveRates.find(worker.m_currentValveId)->second;
+                    }
+                }
             }
 
-            std::map<int, int> const& srcDirectPaths = directPaths.find(currentValveId)->second;
-            std::set<int> potentialValveIds = closedValves;
-
-            int bestPressureAmount = currentPressureAmount;
-            for (int const nextValveId : potentialValveIds)
+            // Return the amount of pressure released up to now
+            if (nextRemainingTime == 0)
             {
-                auto const it = srcDirectPaths.find(nextValveId);
-                int const pathCost = it != srcDirectPaths.end() ? it->second : remainingTime;
-                int const nextRemainingTime = remainingTime - (pathCost + 1);
-
-                if (nextRemainingTime > 0)
+                int totalReleasedPressure = 0;
+                for (Worker const& worker : workers)
                 {
-                    int const valveRate = m_rates.find(nextValveId)->second;
+                    totalReleasedPressure += worker.m_releasedPressure;
+                }
+                return totalReleasedPressure;
+            }
+
+            // Process available workers with the current state
+            std::set<int> potentialValveIds = closedValves;
+            int bestPressureAmount = 0;
+
+            for (Worker& worker : workers)
+            {
+                if (worker.m_remainingTravelTime > 0)
+                {
+                    continue;
+                }
+
+                int const currentValveId = worker.m_currentValveId;
+                std::map<int, int> const& srcDirectPaths = m_directPaths.find(currentValveId)->second;
+
+                for (int const nextValveId : potentialValveIds)
+                {
+                    auto const it = srcDirectPaths.find(nextValveId);
+                    if (it == srcDirectPaths.end())
+                    {
+                        continue;
+                    }
+
+                    worker.m_currentValveId = nextValveId;
+                    worker.m_remainingTravelTime = it->second + 1;
 
                     closedValves.erase(nextValveId);
-                    int const pressureAmount = FindBestPressureReleaseAmount(currentPressureAmount + nextRemainingTime * valveRate, nextRemainingTime, nextValveId, closedValves, directPaths);
+                    int const pressureAmount = FindBestPressureRelease(nextRemainingTime, workers, closedValves);
                     closedValves.insert(nextValveId);
 
                     if (pressureAmount > bestPressureAmount)
@@ -177,9 +213,24 @@ namespace Year2022
                         bestPressureAmount = pressureAmount;
                     }
                 }
+
+                worker.m_currentValveId = currentValveId;
+                worker.m_remainingTravelTime = 0;
             }
 
             return bestPressureAmount;
+        }
+
+        bool IsAnyWorkerAvailable(std::vector<Worker> const& workers) const
+        {
+            for (Worker const& worker : workers)
+            {
+                if (worker.m_remainingTravelTime == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     };
 }
