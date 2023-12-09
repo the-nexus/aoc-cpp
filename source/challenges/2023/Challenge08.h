1ed93e3f681c96244c0ab25a1eb18d715d622fe3
Kevin Pulgar-Turcotte <kevin.pulgarturcotte@gmail.com>
2023-12-08 19:41:14 -0500

    Completed part 1 of day 8 of 2023, need to optim to get part 2 working -.-

---
+++ source/challenges/2023/Challenge08.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include "../ChallengeAbstract.h"
+#include <map>
+
+namespace Year2023
+{
+    class Challenge08 : public ChallengeAbstract
+    {
+    private:
+        typedef unsigned int NodeId;
+
+        struct Node
+        {
+            std::string m_label = "AAA";
+            NodeId m_leftId = 0;
+            NodeId m_rightId = 0;
+        };
+
+        std::map<NodeId, Node> m_nodeMap;
+        std::string m_path;
+
+    public:
+        virtual void SetUp(std::vector<std::string> const& inputLines) override
+        {
+            auto it = inputLines.begin();
+            m_path = *it;
+
+            for (it += 2; it < inputLines.cend(); ++it)
+            {
+                Node node;
+                node.m_label = it->substr(0, 3);
+                node.m_leftId = GetNodeId(it->substr(7, 3));
+                node.m_rightId = GetNodeId(it->substr(12, 3));
+
+                NodeId const id = GetNodeId(node.m_label);
+                m_nodeMap.emplace(id, node);
+            }
+        }
+
+        virtual void Run_PartOne() override
+        {
+            NodeId currentNodeId = GetNodeId("AAA");
+            NodeId targetNodeId = GetNodeId("ZZZ");
+
+            size_t stepCount = 0;
+            //while (currentNodeId != targetNodeId)
+            //{
+            //    char const token = m_path[stepCount++ % m_path.size()];
+            //    currentNodeId = token == 'L' ? m_nodeMap[currentNodeId].m_leftId : m_nodeMap[currentNodeId].m_rightId;
+            //}
+
+            std::cout << stepCount << std::endl;
+        }
+
+        virtual void Run_PartTwo() override
+        {
+            std::vector<NodeId> currentNodeIds;
+            currentNodeIds.reserve(m_nodeMap.size());
+            for (std::pair<NodeId, Node> nodePair : m_nodeMap)
+            {
+                if (nodePair.first % 100 == 0) // id == ??A
+                {
+                    currentNodeIds.push_back(nodePair.first);
+                }
+            }
+
+            size_t stepCount = 0;
+            bool areAllNodesAtTarget = false;
+
+            while (!areAllNodesAtTarget)
+            {
+                char const token = m_path[stepCount++ % m_path.size()];
+                areAllNodesAtTarget = true;
+
+                for (NodeId& currentNodeId : currentNodeIds)
+                {
+                    currentNodeId = token == 'L' ? m_nodeMap[currentNodeId].m_leftId : m_nodeMap[currentNodeId].m_rightId;
+                    areAllNodesAtTarget &= currentNodeId % 100 == 25; // id == ??Z
+                }
+            }
+
+            std::cout << stepCount << std::endl;
+        }
+
+        virtual void CleanUp() override {}
+
+    private:
+        static NodeId GetNodeId(std::string const& nodeLabel)
+        {
+            NodeId nodeId = 0;
+            for (char const token : nodeLabel)
+            {
+                nodeId = nodeId * 100 + static_cast<int>(token - 'A');
+            }
+            return nodeId;
+        }
+    };
+}
