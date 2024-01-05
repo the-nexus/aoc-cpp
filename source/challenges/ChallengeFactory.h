09902eea81e301aacd550ea682943da2aedaeb9a
Kevin Pulgar-Turcotte <kevin.pulgarturcotte@gmail.com>
2024-01-04 20:30:41 -0500

    Removed all files for days that haven't been started

--- source/challenges/ChallengeFactory.h
+++ source/challenges/ChallengeFactory.h
@@ -10,12 +10,7 @@
 // ================================================================================================
 
 //#include "XXXX/YearXXXX.h"
-#include "2015/Year2015.h"
-#include "2016/Year2016.h"
-#include "2017/Year2017.h"
 #include "2018/Year2018.h"
-#include "2019/Year2019.h"
-#include "2020/Year2020.h"
 #include "2021/Year2021.h"
 #include "2022/Year2022.h"
 #include "2023/Year2023.h"
@@ -28,12 +23,7 @@ namespace ChallengeFactory
         switch (year)
         {
         //case XXXX: return YearXXXX::MakeChallenge(day, inputLines);
-        case 2015: return Year2015::MakeChallenge(day, inputLines);
-        case 2016: return Year2016::MakeChallenge(day, inputLines);
-        case 2017: return Year2017::MakeChallenge(day, inputLines);
         case 2018: return Year2018::MakeChallenge(day, inputLines);
-        case 2019: return Year2019::MakeChallenge(day, inputLines);
-        case 2020: return Year2020::MakeChallenge(day, inputLines);
         case 2021: return Year2021::MakeChallenge(day, inputLines);
         case 2022: return Year2022::MakeChallenge(day, inputLines);
         case 2023: return Year2023::MakeChallenge(day, inputLines);
