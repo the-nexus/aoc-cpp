a7d3e96ae21b1be1889fb3b4d1896ea80e71c145
Kevin Pulgar-Turcotte <kevin.pulgarturcotte@gmail.com>
2024-09-09 12:56:09 -0400

    Completed 2019/01 and added temp folder to gitignore so that local tests can be stored without being comitted

---
+++ src/challenges/challenges/2019/Challenge_2019_1.hpp
@@ -0,0 +1,51 @@
+
+#pragma once
+
+#include "challenges/Challenge.hpp"
+
+namespace aoc::challenges
+{
+    class Challenge_2019_1 final : public Challenge<2019, 1>
+    {
+        using Super = Challenge<2019, 1>;
+
+    public:
+        Challenge_2019_1(std::vector<std::string>&& inputLines)
+            : Super(std::move(inputLines)) 
+        {}
+
+        void RunPartOne(std::ostream& outAnswer) override
+        {
+            unsigned int totalFuel = 0;
+            for (std::string const& inputLine : GetInputLines())
+            {
+                int weight = std::stoi(inputLine);
+                totalFuel += static_cast<unsigned int>(GetRequiredFuel(weight));
+            }
+
+            outAnswer << totalFuel;
+        }
+
+        void RunPartTwo(std::ostream& outAnswer) override 
+        {
+            unsigned int totalFuel = 0;
+            for (std::string const& inputLine : GetInputLines())
+            {
+                int weight = std::stoi(inputLine);
+                while (weight > 0)
+                {
+                    weight = GetRequiredFuel(weight);
+                    if (weight > 0)
+                    {
+                        totalFuel += weight;
+                    }
+                }
+            }
+
+            outAnswer << totalFuel;
+        }
+
+    private:
+        static int GetRequiredFuel(int const weight) { return (weight / 3) - 2; }
+    };
+}
