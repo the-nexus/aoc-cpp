963ea61c19cff9b42edefeac718e047ac1e2a933
Kevin Pulgar-Turcotte <kevin.pulgarturcotte@gmail.com>
2024-12-22 16:35:41 -0500

    Completed day 22 of 2024

--- src/challenges/challenges/2024/Challenge_2024_22.hpp
+++ src/challenges/challenges/2024/Challenge_2024_22.hpp
@@ -13,7 +13,29 @@ namespace aoc::challenges
     class Challenge_2024_22 final : public Challenge<2024, 22>
     {
         using Super = Challenge<2024, 22>;
-        using Sequence = std::tuple<int, int, int, int>;
+        struct Sequence
+        {
+            int _data[4] = { 0, 0, 0, 0 };
+
+            int& operator[](size_t const index) { return _data[index]; }
+            int const& operator[](size_t const index) const { return _data[index]; }
+
+            bool operator==(Sequence const& other) const
+            {
+                return _data[0] == other[0]
+                    && _data[1] == other[1]
+                    && _data[2] == other[2]
+                    && _data[3] == other[3];
+            }
+
+            bool operator<(Sequence const& other) const
+            {
+                if (_data[0] != other[0]) return _data[0] < other[0];
+                if (_data[1] != other[1]) return _data[1] < other[1];
+                if (_data[2] != other[2]) return _data[2] < other[2];
+                return _data[3] < other[3];
+            }
+        };
 
     public:
         Challenge_2024_22(std::vector<std::string>&& inputLines)
@@ -24,27 +46,26 @@ namespace aoc::challenges
         {
             std::vector<std::string> const& lines = GetInputLines();
 
-            std::vector<std::future<void>> allSecretNumbersTasks;
+            std::vector<std::future<unsigned int>> allSecretNumbersTasks;
             allSecretNumbersTasks.reserve(lines.size());
-
-            std::vector<std::vector<unsigned int>> allSecretNumbers;
-            allSecretNumbers.resize(lines.size());
 
             for (size_t taskIndex = 0u; taskIndex < lines.size(); ++taskIndex)
             {
-                auto task = [](unsigned int const initialSecretNumber, std::vector<unsigned int>& outSecretNumbers)
+                auto task = [](unsigned int const initialSecretNumber)
                 {
-                    GenerateSecretNumbers(initialSecretNumber, outSecretNumbers);
+                    std::vector<unsigned int> secretNumbers;
+                    GenerateSecretNumbers(initialSecretNumber, secretNumbers);
+                    return secretNumbers.back();
                 };
 
-                allSecretNumbersTasks.emplace_back(std::async(task, std::stoul(lines[taskIndex]), std::ref(allSecretNumbers[taskIndex])));
+                allSecretNumbersTasks.emplace_back(std::async(task, std::stoul(lines[taskIndex])));
             }
 
             unsigned long long sum = 0;
-            for (size_t taskIndex = 0u; taskIndex < allSecretNumbersTasks.size(); ++taskIndex)
+            for (std::future<unsigned int>& task : allSecretNumbersTasks)
             {
-                allSecretNumbersTasks[taskIndex].wait();
-                sum += allSecretNumbers[taskIndex].back();
+                task.wait();
+                sum += task.get();
             }
 
             outAnswer << sum;
@@ -52,16 +73,83 @@ namespace aoc::challenges
 
         void RunPartTwo(std::ostream& outAnswer) override
         {
-            Super::RunPartTwo(outAnswer);
+            std::vector<std::string> const& lines = GetInputLines();
+
+            std::vector<std::future<void>> allProfitSequencesTasks;
+            allProfitSequencesTasks.reserve(lines.size());
+
+            std::vector<std::map<Sequence, int>> allProfitSequences;
+            allProfitSequences.resize(lines.size());
+
+            for (size_t taskIndex = 0u; taskIndex < lines.size(); ++taskIndex)
+            {
+                auto task = [](unsigned int const initialSecretNumber, std::map<Sequence, int>& outProfitSequences)
+                {
+                    std::vector<unsigned int> secretNumbers;
+                    GenerateSecretNumbers(initialSecretNumber, secretNumbers);
+                    EvaluateProfitSequences(secretNumbers, outProfitSequences);
+                };
+
+                allProfitSequencesTasks.emplace_back(std::async(task, std::stoul(lines[taskIndex]), std::ref(allProfitSequences[taskIndex])));
+            }
+
+            std::set<Sequence> uniqueSequences;
+            for (size_t taskIndex = 0u; taskIndex < allProfitSequencesTasks.size(); ++taskIndex)
+            {
+                allProfitSequencesTasks[taskIndex].wait();
+                for (std::pair<Sequence, int> const& profitSequence : allProfitSequences[taskIndex])
+                {
+                    if (!uniqueSequences.contains(profitSequence.first))
+                    {
+                        uniqueSequences.emplace(profitSequence.first);
+                    }
+                }
+            }
+
+            std::vector<std::future<int>> allTotalProfitTasks;
+            allTotalProfitTasks.reserve(uniqueSequences.size());
+
+            for (Sequence const& uniqueSequence : uniqueSequences)
+            {
+                auto task = [&allProfitSequences](Sequence const& uniqueSequence)
+                {
+                    int profit = 0;
+                    for (std::map<Sequence, int> const& profitSequences : allProfitSequences)
+                    {
+                        auto const it = profitSequences.find(uniqueSequence);
+                        if (it != std::cend(profitSequences))
+                        {
+                            profit += it->second;
+                        }
+                    }
+                    return profit;
+                };
+
+                allTotalProfitTasks.emplace_back(std::async(task, uniqueSequence));
+            }
+
+            int bestProfit = 0;
+            for (std::future<int>& task : allTotalProfitTasks)
+            {
+                task.wait();
+                int const profit = task.get();
+                if (profit > bestProfit)
+                {
+                    bestProfit = profit;
+                }
+            }
+
+            outAnswer << bestProfit;
         }
 
         static void GenerateSecretNumbers(unsigned int secretNumber, std::vector<unsigned int>& outSecretNumbers)
         {
-            size_t constexpr amountToGenerate = 2000;
+            size_t constexpr amountToGenerate = 2001;
 
             outSecretNumbers.resize(amountToGenerate);
+            outSecretNumbers[0] = secretNumber;
 
-            for (size_t numberIndex = 0u; numberIndex < amountToGenerate; ++numberIndex)
+            for (size_t numberIndex = 1u; numberIndex < amountToGenerate; ++numberIndex)
             {
                 secretNumber = GenerateSecretNumber(secretNumber);
                 outSecretNumbers[numberIndex] = secretNumber;
@@ -85,6 +173,34 @@ namespace aoc::challenges
             secretNumber = (secretNumber ^ thirdModifier) % pruner;
 
             return secretNumber;
+        }
+
+        static void EvaluateProfitSequences(std::vector<unsigned int> const& secretNumbers, std::map<Sequence, int>& outProfitSequences)
+        {
+            std::vector<int> prices;
+            prices.resize(secretNumbers.size());
+            for (size_t numberIndex = 0u; numberIndex < secretNumbers.size(); ++numberIndex)
+            {
+                prices[numberIndex] = static_cast<int>(secretNumbers[numberIndex] % 10u);
+            }
+
+            std::vector<int> priceChanges;
+            priceChanges.resize(prices.size() - 1u);
+            for (size_t priceIndex = 1u; priceIndex < prices.size(); ++priceIndex)
+            {
+                priceChanges[priceIndex - 1u] = prices[priceIndex] - prices[priceIndex - 1u];
+            }
+
+            Sequence currentSequence = { 0, priceChanges[0], priceChanges[1], priceChanges[2] };
+            for (size_t priceChangeIndex = 3u; priceChangeIndex < priceChanges.size(); ++priceChangeIndex)
+            {
+                std::swap(currentSequence[0], currentSequence[1]);
+                std::swap(currentSequence[1], currentSequence[2]);
+                std::swap(currentSequence[2], currentSequence[3]);
+                currentSequence[3] = priceChanges[priceChangeIndex];
+
+                outProfitSequences.try_emplace(currentSequence, prices[priceChangeIndex + 1u]);
+            }
         }
     };
 }
